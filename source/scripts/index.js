f88ff93148fdc8adce84b1f920793f5766e34b39
vic-knyazeva <v.b.knyazeva@hotmail.com>
2024-11-05 15:07:29 +0700

    main navigation
    Добавляет скрипт для открытия/закрытия меню в мобильной версии

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,18 @@
-/* в этот файл добавляет скрипты*/
+const navMain = document.querySelector('.navigation');
+const navToggle = document.querySelector('.header__toggle');
+
+navMain.classList.remove('navigation--nojs');
+
+navToggle.addEventListener('click', () => {
+  if (navMain.classList.contains('navigation--closed') && navToggle.classList.contains('header__toggle--open')) {
+    navMain.classList.remove('navigation--closed');
+    navMain.classList.add('navigation--opened');
+    navToggle.classList.remove('header__toggle--open');
+    navToggle.classList.add('header__toggle--close');
+  } else {
+    navMain.classList.add('navigation--closed');
+    navMain.classList.remove('navigation--opened');
+    navToggle.classList.add('header__toggle--open');
+    navToggle.classList.remove('header__toggle--close');
+  }
+});
