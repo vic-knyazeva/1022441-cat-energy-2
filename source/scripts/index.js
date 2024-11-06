a81471a55c62577bb41d5734430d0c1d42dfa7a3
vic-knyazeva <v.b.knyazeva@hotmail.com>
2024-11-07 00:09:12 +0700

    fix: nojs styles and script

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,18 +1,26 @@
 const navMain = document.querySelector('.navigation');
 const navToggle = document.querySelector('.header__toggle');
+const productsTitle = document.querySelector('.products');
 
 navMain.classList.remove('navigation--nojs');
+navToggle.classList.remove('header__toggle--nojs');
 
-navToggle.addEventListener('click', () => {
-  if (navMain.classList.contains('navigation--closed') && navToggle.classList.contains('header__toggle--open')) {
-    navMain.classList.remove('navigation--closed');
-    navMain.classList.add('navigation--opened');
-    navToggle.classList.remove('header__toggle--open');
-    navToggle.classList.add('header__toggle--close');
-  } else {
-    navMain.classList.add('navigation--closed');
-    navMain.classList.remove('navigation--opened');
-    navToggle.classList.add('header__toggle--open');
-    navToggle.classList.remove('header__toggle--close');
-  }
-});
+if (productsTitle !== null) {
+  productsTitle.classList.remove('products--nojs');
+}
+
+if (navMain !== null && navToggle !== null) {
+  navToggle.addEventListener('click', () => {
+    if (navMain.classList.contains('navigation--opened')) {
+      navMain.classList.remove('navigation--opened');
+      navMain.classList.add('navigation--closed');
+      navToggle.classList.remove('header__toggle--close');
+      navToggle.classList.add('header__toggle--open');
+    } else {
+      navMain.classList.remove('navigation--closed');
+      navMain.classList.add('navigation--opened');
+      navToggle.classList.remove('header__toggle--open');
+      navToggle.classList.add('header__toggle--close');
+    }
+  });
+}
